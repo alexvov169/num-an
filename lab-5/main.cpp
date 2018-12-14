5f9bb3f06bbb6392ab7a694dd7a21e28d76f6db5
Olexiy Vovchok <a.vovchok.169@gmail.com>
2018-12-14 23:49:44 +0200

    lab-5 cleared

--- lab-5/main.cpp
+++ lab-5/main.cpp
@@ -1,230 +1,169 @@
-#define _USE_MATH_DEFINES
-
 #include <math.h>
-#include <iomanip>
 #include <iostream>
-
-#include "simpson.h"
-#include "matrix.h"
+#include <fstream>
+#include <functional>
 
 using namespace std;
 
-double f(double x){
-    return 1/x - .1*x*x * sin(2*x);
-}
-
-template<typename ValTy, typename IntTy>
-class legendre_iterator_impl {
-public:
-    legendre_iterator_impl(ValTy previous, ValTy current, IntTy n):
-        previous(previous), current(previous), n(n), x(current) {}
-
-    legendre_iterator_impl& operator++() {
-        next = (2*n + 1) / (n+1) * x * current - n / (n+1) * previous;
-        previous = current;
-        current = next;
-        ++n;
-        return *this;
-    }
-
-    const ValTy& operator*() const { return current; }
-
-private:
-    ValTy previous;
-    ValTy current;
-    ValTy next;
-    IntTy n;
-    ValTy x;
-};
-
-template<typename ValTy, typename IntTy>
-legendre_iterator_impl<ValTy, IntTy>
-make_legendre(ValTy previous, ValTy current, IntTy n) {
-    return { previous, current, n };
-}
-
-double L(int n, double x){
-    double Ln_next, Ln = x, Ln_first = 1;
-
-    if (n == 0) return Ln_first;
-    if (n == 1) return Ln;
-
-    int i = 1;
-    while (i < n){
-        Ln_next = (1.0 * (2 * i + 1) / (i + 1)) * x * Ln - (1.0 * i / (i + 1)) * Ln_first;
-        Ln_first = Ln;
-        Ln = Ln_next;
-        ++i;
-    }
-    return Ln;
-}
-
-double func(int N, double t, int k1, int k2, double a, double b){
-    double x = (2 * t - a - b) / (b - a);
-    if (k2==N)
-        return  f(t)*L(k1, x);
-    else
-        return  L(k1, x)*L(k2, x);
-}
-
-double simpson(int k1, int k2, double a, double b, int n, int N) {
+double simpson(double a, double b, double eps, function<double(double)> y) {
     int i;
     double h;
     double sig1 = 0;
     double sig2 = 0;
-    double y0 = func(N, a, k1, k2, a, b), yn = func(N, b, k1, k2, a, b);
-
+    int n = (int)(1.0 / sqrt(eps));
+    double y0 = y(a), yn = y(b);
     h = (b - a) / n;
-    for (i = 1; i < n; i++) {
+        for (i = 1; i < n; i++) {
         if (i % 2 == 0)
-            sig2 += func(N, a + i*h, k1, k2, a, b);
+            sig1 += y(a + i*h);
         else
-            sig1 += func(N, a + i*h, k1, k2, a, b);
+            sig2 += y(a + i*h);
     }
+        double cur_int, prev_int = h / 3 * (4 * sig2 + 2 * sig1 + y0 + yn), cur_even = sig2 + sig1;
+        int cur_n = 2 * n;
+        do{
 
-    return h / 3 * (2 * sig2 + 4 * sig1 + y0 + yn);
+            h = (b - a) / cur_n;
+            double cur_odd = 0, xi;
+            for (i = 1, xi = a + h; i < cur_n; i += 2, xi += 2 * h) {
+                cur_odd += y(xi);
+            }
+            cur_int = h / 3 * (4 * cur_odd + 2 * cur_even + y0 + yn);
+            prev_int = cur_int;
+            cur_even = cur_odd + cur_even;
+            cur_n *= 2;
+        } while (!(((abs((cur_int - prev_int) / cur_int)) < eps) /*&& ((abs(cur_int - prev_int) / 15) < eps)*/));
+    return cur_int;
 }
 
-double Integral(int k1, int k2, double a, double b, int N){
-    double r, In, I2n;
-    double eps = 1e-8;
-    int n = (int)ceil((b - a) / sqrt(sqrt(eps)));
+double chebyshev(int n, double x) {
+    double Tn1, Tn = x, Tn_1 = 1;
 
-    In = simpson(k1, k2, a, b, n, N);
-    I2n = simpson(k1, k2, a, b, 2 * n, N);
-    r = fabs(In - I2n) / 15;
+    if (n == 0) return Tn_1;
 
-    while (r > eps) {
-        In = I2n;
-        n *= 2;
-        I2n = simpson(k1, k2, a, b, n, N);
-        r = fabs(In - I2n) / 15;
+    int i = 1;
+    while (i < n) {
+        Tn1 = 2 * x * Tn - Tn_1;
+        Tn_1 = Tn;
+        Tn = Tn1;
+        ++i;
     }
-    return I2n;
+
+    return Tn;
 }
 
-double* scheme_selection_main_element(double **matrix, int N){
-    int i, j, line, row, k, line_new = 0;
-    double R, max, *M = new double[N];
-    double *res = new double[N];
-    double ** new_matrix = new double*[N];
-    for (int i = 0; i < N + 1; i++)
-        new_matrix[i] = new double[N + 1];
 
-    for (k = 0; k < N - 1; k++){
-
-        max = 0;
-        for (i = 0; i < N; i++)
-            for (j = 0; j < N; j++)
-                if (fabs(matrix[i][j]) > max){
-            max = fabs(matrix[i][j]);
-            line = i;
-            row = j;
-                }
-
-        for (j = 0; j < N + 1; j++)
-            new_matrix[line_new][j] = matrix[line][j];
-        line_new++;
-
-        for (i = 0; i < N; i++)
-            M[i] = -(matrix[i][row] / max);
-
-        for (i = 0; i < N; i++)
-            for (j = 0; j < N + 1; j++)
-                if (i != line)
-                    matrix[i][j] += matrix[line][j] * M[i];
-
-        for (j = 0; j < N + 1; j++)
-            matrix[line][j] = 0;
-
-        for (i = 0; i < N; i++)
-            matrix[i][row] = 0;
+double* gaussian_elimination(double **AM, int N) {
+    int i, j, k, l;
+    double temp1, temp2, temp3;
+    double* result = new double[N];
+    for (j = 0; j < N; j++) {
+        temp1 = AM[j][j];
+        for (i = j; i < N + 1; i++) {
+            AM[j][i] /= temp1;
+        }
+        for (k = j+1; k < N; k++) {
+            temp2 = AM[k][j];
+            for (l = j; l < N + 1; l++) {
+                AM[k][l] -= AM[j][l]*temp2;
+            }
+        }
     }
-
-    for (i = 0; i < N; i++)
-        if (matrix[i][N] != 0)
-            line = i;
-
-    for (j = 0; j < N + 1; j++)
-        new_matrix[line_new][j] = matrix[line][j];
-
-    for (j = 0; j < N; j++)
-        res[j] = 0;
-
-    for (i = N - 1; i >= 0; i--){
-        R = new_matrix[i][N];
-        for (j = 0; j < N; j++)
-            if ((new_matrix[i][j] != 0) && (res[j] != 0))
-                R -= new_matrix[i][j] * res[j];
-        for (j = 0; j < N; j++)
-            if ((new_matrix[i][j] != 0) && (res[j] == 0))
-                res[j] = R / new_matrix[i][j];
+    for (j = N - 1; j >= 0; j--) {
+        temp3 = 0;
+        for (i = N - 1; i > j; i--) {
+            temp3 += AM[j][i] * result[i];
+        }
+        result[j] = AM[j][N] - temp3;
     }
+    return result;
+}
 
+double * makeA(double a, double b, double eps, int N, function<double(double)> y) {
+    double ** A = new double*[N+1];
+    int j, i;
+    for (j = 0; j < N+1; j++) {
+        A[j] = new double[N + 2];
+    }
+    for (j = 0; j < N+1; j++) {
+        for (i = 0; i < N+1; i++) {
+            if (j != i)
+                A[j][i] = A[i][j] = simpson(a, b, eps, [&](double x) {return chebyshev(j, x) * chebyshev(i, x);});
+            else
+                A[j][i] = simpson(a, b, eps, [&](double x) {return chebyshev(j, x) * chebyshev(i, x);});
+        }
+        A[j][N+1] = simpson(a, b, eps, [&](double x) {return chebyshev(j, x) * y(x);});
+    }
+    auto res = gaussian_elimination(A, N+1);
     return res;
 }
 
-double * make_A(double a, double b, int N){
-    int i, j;
-    double *result = new double[N];
-    double ** A = new double*[N];
-    for (int i = 0; i < N + 1; i++)
-        A[i] = new double[N + 1];
+class LeastSqAppr {
+    double * A;
+    int N;
+public:
+    LeastSqAppr(function<double(double)> y, double a, double b, double eps, int N) {
+        A = makeA(a, b, eps, N, y);
+        this->N = N;
+    }
+    double operator()(double x) {
+        double Tn1, Tn = x, Tn_1 = 1, sum;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N + 1; j++) {
-            A[i][j] = A[j][i] = Integral(i, j, a, b, N);
+        sum = Tn_1*A[0];
+        if (N == 0) return sum;
+        sum += Tn*A[1];
+        int i = 1;
+        while (i < N+1) {
+            Tn1 = 2 * x * Tn - Tn_1;
+            Tn_1 = Tn;
+            Tn = Tn1;
+            sum += Tn*A[i+1];
+            ++i;
         }
-        A[i][N] = Integral(i, N, a, b, N);
+
+        return sum;
     }
+};
 
-    result = scheme_selection_main_element(A, N);
-    return result;
+int main() {
+    auto f = [&](double x) {return 7.5 * log10(x) * sin(x); };
+    auto g = [&](double x) {return 1/x - 0.1 * x * x * sin(2*x); };
 
-}
-
-double Pm(double x, double a, double b, int n){
-    double *A = make_A(a, b, n);
-    double  t = (2 * x - b - a) / (b - a);
-    double y = 0;
-    auto L = make_legendre(1., t, 0);
-    for (int j = 0; j < n; ++j) {
-        y += A[j] * *L;//(j, t);
-        ++L;
-    }
-    return y;
-}
-
-int main(){
-    double a = 20, b = 30;
-    double eps = 0.01;
-
-    int k = (int)(b - a) * 2;//кількість точок, які обчислюються на заданому інтервалі
-    double y, x, step = (b - a) / k;
-    int n = 2;
-    double Dev; // Deviation - середнє квадратичне відхилення
-
-    do{
-        n += 1;
-        Dev = 0;
-        x = a;
-        for (int i = 0; i <= k; ++i){
-            y = f(x) - Pm(x, a, b, n);
-            Dev += y*y;
-            x += step;
+    double a = 2, b = 11;
+    int N = 10;
+    double result_n;
+    double exact;
+    int n = 9;
+    double h = (b - a) / n, deviation_sum, delta;
+    double x;
+    double epsilon = 0.01;
+    do {
+        deviation_sum = 0;
+        for (int i = 1; i < n; ++i) {
+            x = a + h*i;
+            exact = g(x);
+            LeastSqAppr least(g, a, b, 0.00001, N);
+            result_n = least(x);
+            delta = abs(result_n - exact);
+            deviation_sum = delta*delta + deviation_sum;
+            printf("%.6f %.6f %.6f\n", x, least(x), g(x));
+            //cout << "N = " << N << " exact " << exact << " apprx " << result_n  << endl;
         }
-        Dev = sqrt(Dev / (k+1));
-        cout << "Deviation[" << n << "] = " << Dev << endl;
-    } while (Dev > eps);
-
-    cout << "N = " << n << endl << endl;
-
-    cout << "   X          P(x)" << endl;
-    for (double i = a; i <= b; i += step){
-        double h = Pm(i, a, b, n);
-        cout << setw(7) << fixed << setprecision(2) << i;
-        cout << setw(15) << fixed << setprecision(8) << h << endl;
-    }
-
+        cout << "N = " << N << " dev = " << (sqrt(deviation_sum / (n + 1))) << endl;
+        ++N;
+    } while (sqrt(deviation_sum / (n + 1)) > epsilon);
+    /*
+    for (int ord = 10; ord < 30; ord++) {
+        LeastSqAppr least(g, 1, 10, 0.0000001, 8);
+        if (sqrt((simpson(1, 10, 0.0001, [&](double x) {return (7.5 * log10(x) * sin(x) - least(x)) * (7.5 * log10(x) * sin(x) - least(x)); })) / 9) < 0.1){
+            for (double x = 1; x <= 10; x += 0.5) {
+                printf("%.6f %.6f %.6f\n", x, least(x), g(x));
+            }
+            printf("%f\n", sqrt((simpson(1, 10, 0.0001, [&](double x) {return (7.5 * log10(x) * sin(x) - least(x)) * (7.5 * log10(x) * sin(x) - least(x));})) / 9));
+            break;
+        }
+    }*/
     return 0;
+    //printf("%f\n", chebyshev(3, 5));
+    //printf("%f", simpson(1, 10, 0.001, [&](double x) {return chebyshev(0, x) * chebyshev(1, x); }));
 }
